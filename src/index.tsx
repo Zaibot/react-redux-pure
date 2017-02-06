a03b790d94e707ba6aecc156d5bc0b93f6f0fc34
Tobias de Groen <tdgroen@lias-software.nl>
2017-02-06 13:53:00 +0100

    change(pureconnect): include props in render and dispatch signature.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { connect }from 'react-redux';
+import { connect } from 'react-redux';
 
 class PureComponentWrap extends React.PureComponent<any, any> {
     render() {
@@ -10,13 +10,14 @@ class PureComponentWrap extends React.PureComponent<any, any> {
 
 export const PureConnect = function <TProps>(name: string) {
     return function <TState, TActions>(
-        state: (state) => TState,
-        actions: (dispatch) => TActions, cb: (props: TProps & TState & TActions) => React.ReactElement<TProps>
+        state: (state, props?: TProps) => TState,
+        dispatch: (dispatch, props?: TProps) => TActions,
+        render: (props: TProps & TState & TActions) => React.ReactElement<TProps>
     ): React.StatelessComponent<TProps> {
         const f = function PureFunction(props) {
-            return <PureComponentWrap _inner={cb} {...props} />
+            return <PureComponentWrap _inner={render} {...props} />
         } as any;
         f.displayName = `Connecting(${name})`;
-        return connect(state, actions)(f) as any as React.StatelessComponent<TProps>;
+        return connect(state, dispatch)(f) as any as React.StatelessComponent<TProps>;
     };
 };
