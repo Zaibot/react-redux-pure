b1c23543efc24ff7d585ef3e56bb6926ef13a6a2
Tobias de Groen <tdgroen@zaibot.net>
2018-02-23 11:13:14 +0100

    refactor(project): extend typings

--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,55 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-const nameFunction = (name, fn) => new Function(`fn`, `return function ${name}() { return fn.apply(this, arguments) };`)(fn);
+const nameFunction = <T extends Function>(name: string, fn: T) => new Function(`fn`, `return function ${name}() { return fn.apply(this, arguments) };`)(fn) as T;
 
-class PureComponentWrap extends React.PureComponent<any, any> {
+class PureComponentWrap<TProps> extends React.PureComponent<TProps> {
     render() {
-        const { _inner, ...others } = this.props;
+        const { _inner, ...others } = this.props as any;
         return _inner(others);
     }
 }
 
 export const Connect = function <TProps>(name: string) {
-    return function <TState, TActions>(
+    return <TState extends {}, TActions extends {}>(
         state: (state, props?: TProps) => TState,
         dispatch: (dispatch, props?: TProps) => TActions,
         render: (props: TProps & TState & TActions & { children?: React.ReactNode }) => React.ReactElement<TProps>
-    ): React.StatelessComponent<TProps> {
-        return connect(state, dispatch)(render) as any as React.StatelessComponent<TProps>;
+    ): React.StatelessComponent<TProps> => {
+        const res = connect(state, dispatch)(render);
+        return res;
     };
 };
 
 export const PureFunctional = function <TProps>(name: string) {
-    return function(
+    return (
         render: (props: TProps & { children?: React.ReactNode }) => React.ReactElement<TProps>
-    ): React.StatelessComponent<TProps> {
-        var f = function PureFunction(props) {
-            return <PureComponentWrap _inner={render} {...props} />
-        } as any;
-
-        if (f) f = nameFunction(`PureFunctional_${name}`, f)
-        f.displayName = `PureFunctional(${name})`;
-
-        return f as any as React.StatelessComponent<TProps>;
+    ): React.StatelessComponent<TProps> => {
+        const component: React.StatelessComponent<TProps> = Object.assign(
+            nameFunction(`PureFunctional_${name}`, function PureFunction(props: TProps) {
+                return <PureComponentWrap _inner={render} {...props} />
+            }), {
+                displayName: `PureFunctional(${name})`,
+            });
+        return component;
     };
 };
 
 export const PureConnect = function <TProps>(name: string) {
-    return function <TState, TActions>(
+    return <TState extends {}, TActions extends {}>(
         state: (state, props?: TProps) => TState,
         dispatch: (dispatch, props?: TProps) => TActions,
         render: (props: TProps & TState & TActions & { children?: React.ReactNode }) => React.ReactElement<TProps>
-    ): React.StatelessComponent<TProps> {
-        var f = function PureFunction(props) {
-            return <PureComponentWrap _inner={render} {...props} />
-        } as any;
-
-        if (state) state = nameFunction(`PureConnect_${name}_state`, state)
-        if (dispatch) dispatch = nameFunction(`PureConnect_${name}_dispatch`, dispatch)
-        if (render) render = nameFunction(`PureConnect_${name}_render`, render)
-        if (f) f = nameFunction(`PureConnect_${name}`, f)
-        f.displayName = `PureConnect(${name})`;
-
-        return connect(state, dispatch)(f) as any as React.StatelessComponent<TProps>;
+    ): React.StatelessComponent<TProps> => {
+        const component: React.StatelessComponent<TProps> = Object.assign(
+            nameFunction(`PureConnect_${name}`, function PureFunction(props: TProps) {
+                return <PureComponentWrap _inner={render} {...props} />
+            }), {
+                displayName: `PureConnect(${name})`,
+            });
+        if (state) state = nameFunction(`PureConnect_${name}_state`, state);
+        if (dispatch) dispatch = nameFunction(`PureConnect_${name}_dispatch`, dispatch);
+        if (render) render = nameFunction(`PureConnect_${name}_render`, render);
+        return connect(state, dispatch)(component);
     };
 };
